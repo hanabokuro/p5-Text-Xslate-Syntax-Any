724d19d8f5eae5df2a8f028b0ecca9d1554d7765
Shigeki Morimoto <github@morimo.to>
2012-10-19 18:32:38 +0900

    initial commit

---
+++ lib/Text/Xslate/Syntax/Any.pm
@@ -0,0 +1,32 @@
+package Text::Xslate::Syntax::Any;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Text::Xslate::Syntax::Any -
+
+=head1 SYNOPSIS
+
+  use Text::Xslate::Syntax::Any;
+
+=head1 DESCRIPTION
+
+Text::Xslate::Syntax::Any is
+
+=head1 AUTHOR
+
+mixi-inc E<lt>shigeki.morimoto@mixi.co.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
